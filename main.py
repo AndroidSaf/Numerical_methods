9fe34bfe7112c5feadbd05bd0f80967e12b56d9f
Andrew <56847185+AndroidSaf@users.noreply.github.com>
2020-11-01 18:01:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,76 @@
-# Main program for solving SLAE
-
 import numpy as np
 import random as rd
-import Gauss_method as f_1
-import Sweep_method as f_2
-import Simple_iteration_method as f_3
+import pandas as pd
+import Gauss_method as file_1
+import Sweep_method as file_2
+import Simple_iteration_method as file_3
 
-l = 'yes'
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 5)
 
-def get_B(A, X):
-    B = np.dot(A, X)
-    return B
+data = {'slay dimension': pd.Series(np.zeros(5),
+                                    index=['1', '2',
+                                           '3', '4', '5']),
+        'sweep methods quantity': pd.Series(np.zeros(5),
+                                            index=['1', '2',
+                                                   '3', '4', '5']),
+        'delta X_min': pd.Series(np.zeros(5),
+                                 index=['1', '2',
+                                        '3', '4', '5']),
+        'delta X_max': pd.Series(np.zeros(5),
+                                 index=['1', '2',
+                                        '3', '4', '5']),
+        'time': pd.Series(np.zeros(5),
+                          index=['1', '2',
+                                 '3', '4', '5'])}
+df = pd.DataFrame(data)
 
-def diag_max(C):
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                C[i, j] = rd.random() + 100
-            else:
-                C[i, j] = rd.random()
-    return C
 
-def three_diag_matrix(C):
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                C[i, j] = rd.random()
-            elif np.abs(i - j) == 1:
-                C[i, j] = rd.random()
-    return C
+f_name = {
+    '1': 'Gauss method',
+    '2': 'Sweep method',
+    '3': 'Simple iteration method'
+}
 
-while l == 'yes':
+dim_sizes = {
+    '1': 5,
+    '2': 10,
+    '3': 50,
+    '4': 100,
+    '5': 1000
+}
 
-    print("------------------------------------------")
-    print("Gauss_method")
-    n = int(input("Enter the number of equations: \n"))
-    A = np.array([rd.random() for i in range(n ** 2)])
-    A = A.reshape(n, n)
-    print("A = \n", A)
-    X = np.array([rd.random() for i in range(n)])
-    print("X = \n", X)
-    B = get_B(A, X)
-    print("B = \n", B)
-    print("dX = \n", X - f_1.Gauss_method(A, B, n))
+iter_q = [10, 10, 10, 10, 2]
 
-    print("------------------------------------------")
-    print("Sweep_method")
-    n = int(input("Enter the number of equations: \n"))
-    C = np.zeros((n, n))
-    A = three_diag_matrix(C)
-    print("A = \n", A)
-    print("X = \n", X)
-    B = get_B(A, X)
-    print("B = \n", B)
-    print("dX = \n", X - f_2.Sweep_method(A, B, n))
+while True:
 
-    print("------------------------------------------")
-    print("Simple_iteration_method")
-    n = int(input("Enter the number of equations: \n"))
-    C = np.zeros((n, n))
-    A = diag_max(C)
-    print("A = \n", A)
-    print("X = \n", X)
-    B = get_B(A, X)
-    print("B = \n", B)
-    print("dX = \n", X - f_3.Simple_iteration_method(A,B,n))
+    print("Choose the method to solve SLAY:")
+    print(" 1. Gauss method\n", "2. Sweep method\n", "3. Simple iteration method")
+    numb = input()
 
-    print("Try again?")
+    while True:
+        print("Choose SLAY dimension:")
+        print(" 1. 5 x 5\n", "2. 10 x 10\n", "3. 50 x 50\n", "4. 100 x 100\n", "5. 1000 x 1000\n")
+        dim = input()
+
+        methods = {
+            '1': file_1.Gauss_method(dim_sizes[dim], iter_q[int(dim) - 1]),
+            '2': file_2.Sweep_method(dim_sizes[dim], iter_q[int(dim) - 1]),
+            '3': file_3.Simple_iteration_method(dim_sizes[dim], iter_q[int(dim) - 1])
+        }
+
+        df.loc[dim] = methods[numb]
+        print(df, "\n")
+        print("Want to change dimension?")
+        k = input()
+        if k != 'yes':
+            break
+
+    print("Want to change method?")
+    df.to_csv('D:\Python\data_clay.csv')
+    df = df.applymap(lambda x: 0)
     l = input()
+    if l != 'yes':
+        break
 
 print("Thank you for attention!")
